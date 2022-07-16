faa11e51e231fd0ecff0e2f0d9f521496aee2a49
Marc Pabst <mail@marcpabst.com>
2022-07-16 09:53:46 +0200

    added preproceccing for imagenet

--- src/datasets/imagenet64x64.jl
+++ src/datasets/imagenet64x64.jl
@@ -13,15 +13,13 @@ Please note that images are loaded and kept (!) in memory.
 struct ImageNet64x64{T <: Integer}
     data::Array{T, 4}
     labels::Vector{Int}
-    datatype::Type{U}
-    normalize::Bool
 end
 
-function ImageNet64x64(path::String; datatype::Type = Float32, demean::Bool = true, normalize::Bool = true) 
+function ImageNet64x64(path::String; demean::Bool = true) 
     # load file
     _f = FileIO.load(path)
 
-    return ImageNet64x64(_f["data"], _f["labels"], datatype, normalize)
+    return ImageNet64x64(_f["data"], _f["labels"])
 end
 
 # for DataLoaders.DataLoader
@@ -33,21 +31,42 @@ getobs(data::ImageNet64x64, i) = @view data.data[:,:,:,i]
 Base.getindex(data::ImageNet64x64, i) = getobs(data, i) 
 Base.length(data::ImageNet64x64) = nobs(data)
 
-function imagenet64x64_preprocess(data::Array{T, 4}; demean = true, normalize = true) where T <: Integer
-    out = convert(Float32, data) ./ 255 
-    out .= demean ? out .- imagenet_means : out
-    out .= normalize ? out ./ imagenet_stds : out
+function imagenet64x64_preprocess(data; demean = true, normalize = true)
+    out = Base.convert.(Float32, data) ./ 255 
+    
+    if demean
+        out[:,:,1,:] .= out[:,:,1,:] .- imagenet_means[1]
+        out[:,:,2,:] .= out[:,:,2,:] .- imagenet_means[2]
+        out[:,:,3,:] .= out[:,:,3,:] .- imagenet_means[3]
+    end
+
+    if normalize 
+        out[:,:,1,:] .= out[:,:,1,:] ./ imagenet_stds[1]
+        out[:,:,2,:] .= out[:,:,2,:] ./ imagenet_stds[2]
+        out[:,:,3,:] .= out[:,:,3,:] ./ imagenet_stds[3]
+    end
+    
+    return out
+end
+
+function imagenet64x64_reverse_preprocess(data; demean = true, normalize = true)
+
+    if normalize 
+        out[:,:,1,:] .= out[:,:,1,:] .* imagenet_stds[1] # R
+        out[:,:,2,:] .= out[:,:,2,:] .* imagenet_stds[2] # G
+        out[:,:,3,:] .= out[:,:,3,:] .* imagenet_stds[3] # B
+    end
+    
+    if demean
+        out[:,:,1,:] .= out[:,:,1,:] .+ imagenet_means[1] # R
+        out[:,:,2,:] .= out[:,:,2,:] .+ imagenet_means[2] # G
+        out[:,:,3,:] .= out[:,:,3,:] .+ imagenet_means[3] # B
+    end
 
     return out
 end
 
-function imagenet64x64_reverse_preprocess(data::Array{Float32, 4}; demean = true, normalize = true)
-    out .= normalize ? out .* imagenet_stds : out
-    out .= demean ? out .+ imagenet_means : out
-    return out
-end
-
-function imagenet64x64_asimage(data::Array{Real, 3})
-    out = convert(Float32, data) ./ 255 
+function imagenet64x64_asimage(data)
+    out = Base.convert.(Float32, data) ./ 255 
     return colorview(RGB, permutedims(out, (3,2,1)))
 end
